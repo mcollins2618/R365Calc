30f1acc35e657143b2ba919b9442f33faf455262
mcollins2618 <mcollins2618@gmail.com>
2018-04-15 09:28:01 -0700

    Initial Commit Problem 1

---
+++ R365CalculatorConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace R365CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Add("\t1,2");
+            Console.WriteLine(input);
+            Console.ReadLine();
+        }
+
+        public static int Add(string text)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            List<int> numbers = new List<int>();
+            string[] words = text.Split(delimiterChars);
+            foreach (var word in words)
+            {
+                if (word != "")
+                {
+                    int stringToInt = Convert.ToInt32(word);
+                    numbers.Add(stringToInt);
+                }
+            }
+            int sum = numbers.Sum();
+            return sum;
+        }
+    }
+}
