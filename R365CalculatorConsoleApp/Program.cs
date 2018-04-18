67789ab4219413f75b6ff3ca9aeee9fd545657e4
mcollins2618 <mcollins2618@gmail.com>
2018-04-17 21:23:55 -0700

    Naming convention changes

--- R365CalculatorConsoleApp/Program.cs
+++ R365CalculatorConsoleApp/Program.cs
@@ -19,7 +19,7 @@ namespace R365CalculatorConsoleApp
             List<int> numbers = new List<int>();
             List<int> negativeNumbers = new List<int>();
             //Default Delimeters
-            char[] defaultDelimiterChars = { '\n', '/', ',', ']' };
+            char[] defaultDelimCharArray = { '\n', '/', ',', ']' };
             char[] delimCharArray; //*****NEW ADDITION - Changed naming convention to be less confusing
             var delimList = new List<char>();
             var totalSum = 0;
@@ -29,38 +29,37 @@ namespace R365CalculatorConsoleApp
                 var delimStart = text[0].Split('\n').First();
                 char[] delimOption = delimStart.Split('/').Last().Distinct().ToArray(); //*****NEW ADDITION - Custom Delim options to be Distinct rather than showing duplicates
                 delimList.AddRange(delimOption);
-                delimList.AddRange(defaultDelimiterChars);
+                delimList.AddRange(defaultDelimCharArray);
                 //Delimeter character array
                 delimCharArray = delimList.ToArray();
             }
             else
             {
-                delimCharArray = defaultDelimiterChars;
+                delimCharArray = defaultDelimCharArray;
             }
-                //*******NEW ADDTION - REMOVE FOR LOOP as it is not needed
-                //Splits the string input to remove any and all delim's
-                string[] words = text[0].Split(delimCharArray);
-                foreach (var word in words)
+            //*******NEW ADDTION - REMOVE FOR LOOP as it is not needed
+            //Splits the string input to remove any and all delim's
+            string[] words = text[0].Split(delimCharArray);
+            foreach (var word in words)
+            {
+                if (word != "")
                 {
-                    if (word != "")
+                    int stringToInt = Convert.ToInt32(word);
+                    //Check and ignore any converted int that is above 1000
+                    if (stringToInt < 1000)
                     {
-                        int stringToInt = Convert.ToInt32(word);
-                        //Check and ignore any converted int that is above 1000
-                        if (stringToInt < 1000)
+                        //Negative Number check. Will add any negative numbers to list which will trigger exception
+                        if (stringToInt < 0)
                         {
-                            //Negative Number check. Will add any negative numbers to list which will trigger exception
-                            if (stringToInt < 0)
-                            {
-                                negativeNumbers.Add(stringToInt);
-                            }
-                            else
-                            {
-                                numbers.Add(stringToInt);
-                            }
+                            negativeNumbers.Add(stringToInt);
+                        }
+                        else
+                        {
+                            numbers.Add(stringToInt);
                         }
                     }
                 }
-            
+            }
             //Try Catch that will throw exception when a negative number is found. 
             //If no negatives are found - totalSum will calculate and be displayed.
             try
